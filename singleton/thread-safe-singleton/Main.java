25c305e38e5344cf8088524aae4e3c13b785dfd4
joeami <i.otkhozoria@yahoo.com>
2020-01-07 21:29:26 +0400

    thread-safe-singleton

---
+++ singleton/thread-safe-singleton/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        if (s1 == s2) {
+            System.out.println("Those two objects are the same");
+        }
+    }
+    
+}
