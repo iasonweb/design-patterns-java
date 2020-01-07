1e1cabe73b6a036e19a5d197a5668de8987d1f4a
joeami <i.otkhozoria@yahoo.com>
2020-01-07 20:36:46 +0400

    added singleton pattern

---
+++ singleton/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        if (singleton1 == singleton2) {
+            System.out.println("Those two objects are the same");
+        }
+    }
+    
+}
